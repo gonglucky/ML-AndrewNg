9bc8e23bed72b2ae0711cac3de4a73e7cc7644f4
gongwf <875862866@qq.com>
2020-09-05 17:20:29 +0800

    吴恩达-机器学习

---
+++ code/testex1/1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 定义fig
+fig = plt.figure(2,1)
+# 建立子图
+ax = fig.subplots()    # 2*1
+
+# 设置子图之间的间距，默认值为1.08
+plt.show()
